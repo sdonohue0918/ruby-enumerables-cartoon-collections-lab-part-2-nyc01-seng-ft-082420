7e47f071363ef528c6e6f4c5024aba0394de70c3
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-12 16:20:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,12 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+  element_index = 0
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
-  planeteer_calls.find {|element| planeteer_calls[index.to_i] == valid_calls[index.to_i]}
+  while element_index < planeteer_calls.length && element_index < valid_calls.length do
+    planeteer_calls.find {|element| planeteer_calls[index.to_i] == valid_calls[index.to_i]}
+  end
+
+  
 end
